8e5a0bf5e8010bde60702a9a0b77ba270626fb45
FelipeZabala <Kael_47@hotmail.com>
2021-03-04 00:43:44 -0300

    Arreglos en el login parte 2

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,15 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _auth: Auth | undefined;
+
+  get auth(): Auth{
+    return { ...this._auth!}
+  }
+
   constructor( private http: HttpClient) { }
 
   login() {
     return this.http.get<Auth>(`http://localhost:3050/admin/5`)
+              .pipe(
+                tap( auth => this._auth = auth)
+
+              );
   }
 }
